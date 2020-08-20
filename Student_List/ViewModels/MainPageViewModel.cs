945f5239c25cba5658b4b3a80b11a5a379d50e6b
MadEldar1 <markskilink@gmail.com>
2020-08-20 19:18:59 +0700

    Student_List

---
+++ Student_List/ViewModels/MainPageViewModel.cs
@@ -0,0 +1,29 @@
+﻿using Student_List.Models;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Student_List.ViewModels
+{
+    public class MainPageViewModel
+    {
+        public ObservableCollection<Student> StudentList { get; set; } = new ObservableCollection<Student>();
+        public MainPageViewModel()
+        {
+            for (int i = 0; i < 50; i++)
+            {
+                var random = new Random();
+                var student = new Student() {
+                    Id = i,
+                    Name = $"Student {i}",
+                    GPA = double.Parse(String.Format("{0:N2}", random.NextDouble() * 4))
+                };
+                StudentList.Add(student);
+            }
+        }
+    }
+}
---
+++ Student_List/Views/MainPage.xaml.cs
@@ -0,0 +1,40 @@
+﻿using Student_List.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace Student_List.Views
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPageViewModel StudentViewModel { get; set; } = new MainPageViewModel();
+        public MainPage()
+        {
+            this.InitializeComponent();
+
+            this.DataContextChanged += (s, e) =>
+            {
+                StudentViewModel = DataContext as MainPageViewModel;
+            };
+
+            Debug.WriteLine(StudentViewModel.StudentList[4].Name);
+        }
+    }
+}
