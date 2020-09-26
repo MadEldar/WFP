852a8b7c65b6947eb618bb4c3328e01d489ef5ab
MadEldar1 <markskilink@gmail.com>
2020-09-26 15:28:51 +0700

    WAD Practice

---
+++ ProductManager/Controllers/ProductController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using ProductManager.Services;
+
+namespace ProductManager.Controllers
+{
+    public class ProductController : Controller
+    {
+        public ProductController(ProductManagerService productService)
+        {
+            ProductService = productService;
+        }
+
+        public ProductManagerService ProductService { get; }
+
+        [Route("/Products")]
+        public IActionResult Index()
+        {
+            return View(ProductService.GetProducts());
+        }
+    }
+}
